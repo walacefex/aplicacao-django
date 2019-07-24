3f3e2fc8d290cbfc13305c02b20c8bf24cfb3ea5
walacefex <walacefex01@gmail.com>
2019-07-24 17:27:04 -0300

    django

---
+++ website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    contexto = {
+
+    }
+    return render(request , 'index.html', contexto)
+
+def sobre(request):
+    contexto = {
+
+    }
+    return render(request, 'sobre.html', contexto)
